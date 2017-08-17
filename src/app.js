b9832ce63d030e0104d18314ed1687bb6e347e34
yuanzhaokang <kangzhaoyuan@gmail.com>
2017-08-17 13:58:09 +0800

    update

--- src/app.js
+++ src/app.js
@@ -1,43 +1,21 @@
 import * as L from 'leaflet';
 import zrender from 'zrender/src/zrender';
-import Circle from 'zrender/src/graphic/shape/Circle';
 
 class LZrender extends L.Layer {
    constructor(options) {
       super(options);
 
-      this.canvas = null;
       this.map = null;
       this.zr = null;
    }
 
    onAdd(map) {
-      this.map = map;
-      console.log(this.map._panes.overlayPane);
+      console.log(map);
 
-      this.zr = zrender.init(this.map._panes.overlayPane, {
-         width: 300,
-         height: 400
+      this.zr = zrender.init(map._panes.overlayPane, {
+         width: getComputedStyle(map._container).width,
+         height: getComputedStyle(map._container).height
       });
-
-      let c = new Circle({
-         shape: {
-            cx: 100,
-            cy: 100,
-            r: 30
-         },
-         style: {
-            fill: 'blue'
-         }
-      });
-
-      c.animateShape(true)
-         .when(1000, {
-            r: 50
-         })
-         .start();
-
-      this.zr.add(c);
    }
 
    onRemove(map) {
@@ -55,12 +33,16 @@ class LZrender extends L.Layer {
    }
 
    beforeAdd(map) {
-      console.log("===");
+      // console.log("===");
    }
 
    addTo(map) {
       map.addLayer(this);
       return this;
+   }
+
+   getZR() {
+      return this.zr;
    }
 }
 
--- src/test.js
+++ src/test.js
@@ -1,5 +1,6 @@
 import * as L from 'leaflet';
 import LZrender from './app';
+import Circle from 'zrender/src/graphic/shape/Circle';
 
 var map = L.map("canvas", { center: [46.81509864599243, 8.3221435546875], zoom: 8 });
 var Esri_WorldTopoMap = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
@@ -10,3 +11,24 @@ Esri_WorldTopoMap.addTo(map);
 let lz = new LZrender();
 // lz.addTo(map);
 lz.addTo(map);
+
+let zr = lz.getZR();
+
+let c = new Circle({
+   shape: {
+      cx: 100,
+      cy: 100,
+      r: 30
+   },
+   style: {
+      fill: 'blue'
+   }
+});
+
+c.animateShape(true)
+   .when(1000, {
+      r: 50
+   })
+   .start();
+
+zr.add(c);
