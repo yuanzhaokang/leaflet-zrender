2983975da4d3c0a758dce98a598121bdabd98d33
yuanzhaokang <2503144553@qq.com>
2017-08-20 17:03:23 +0800

    move end and zrender can repaint the canvas.

--- src/app.js
+++ src/app.js
@@ -1,67 +1,69 @@
 import * as L from 'leaflet';
 import zrender from 'zrender/src/zrender';
+import Group from 'zrender/src/container/Group';
+import Circle from 'zrender/src/graphic/shape/Circle';
 
 class LZrender extends L.Layer {
-   constructor(options) {
-      super(options);
+    constructor(options) {
+        super(options);
 
-      this.map = null;
-      this.draw = options.draw || (() => { });
-   }
+        this.map = null;
+        this.draw = options.draw || (() => { });
+        this.rootGroup = new Group();
+    }
 
-   onAdd(map) {
-      console.log(map);
-      this.map = map;
+    onAdd(map) {
+        this.map = map;
 
-      let zr = zrender.init(map._panes.overlayPane, {
-         width: getComputedStyle(map._container).width,
-         height: getComputedStyle(map._container).height
-      });
+        this.zr = zrender.init(map._panes.overlayPane, {
+            width: getComputedStyle(map._container).width,
+            height: getComputedStyle(map._container).height
+        });
 
-      // let projection = [];
-      this.draw(zr);
-   }
+        this.zr.add(this.rootGroup);
+        this.draw(this.rootGroup);
+    }
 
-   onRemove(map) {
-      if(this.zr) {
-         zr.dispose();
-      }
-   }
+    onRemove(map) {
+        if (this.zr) {
+            zr.dispose();
+        }
+    }
 
-   getEvents() {
-      return {
-         zoomend: this._zoomUpdate,
-         moveend: this._moveUpdate
-      };
-   }
+    getEvents() {
+        return {
+            zoomend: this._zoomUpdate,
+            moveend: this._moveUpdate
+        };
+    }
 
-   getAttribution() {
+    getAttribution() {
 
-   }
+    }
 
-   beforeAdd(map) {
-      // console.log("===");
-   }
+    beforeAdd(map) {
+    }
 
-   addTo(map) {
-      map.addLayer(this);
-      return this;
-   }
+    addTo(map) {
+        map.addLayer(this);
+        return this;
+    }
 
-   _zoomUpdate(event) {
-      console.log(this.map.getZoom());
-      console.log(event);
-   }
+    _zoomUpdate(event) {
+        console.log(this.map.getZoom());
+        console.log(event);
+    }
 
-   _moveUpdate(event) {
-      console.log(this.map);
-      let translate = getComputedStyle(this.map._mapPane).transform.replace('matrix', '').replace('(', '').replace(')', '').split(',');
-      let translateX = translate[4];
-      let translateY = translate[5];
+    _moveUpdate(event) {
+        let translate = getComputedStyle(this.map._mapPane).transform.replace('matrix', '').replace('(', '').replace(')', '').split(',');
+        let translateX = parseFloat(translate[4].trim());
+        let translateY = parseFloat(translate[5].trim());
 
-      this.map._panes.overlayPane.style.transform = `translate(${-translateX}px, ${-translateY}px)`
-      console.log(translateX, translateY);
-   }
+        this.rootGroup.position = [translateX, translateY];
+        // Use dirty function and zrender will repaint.
+        this.rootGroup.dirty();
+        this.map._panes.overlayPane.style.transform = `translate(${-translateX}px, ${-translateY}px)`
+    }
 }
 
 // L.Zrender = function (callback, options) {
--- src/test.js
+++ src/test.js
@@ -4,33 +4,35 @@ import Circle from 'zrender/src/graphic/shape/Circle';
 
 var map = L.map("canvas", { center: [39, 116], zoom: 8 });
 var Esri_WorldTopoMap = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
-   attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
+    attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
 });
 Esri_WorldTopoMap.addTo(map);
 
 let lz = new LZrender({
-   draw: draw
+    draw: draw
 });
 
 lz.addTo(map);
 
-function draw(zr) {
-   let c = new Circle({
-      shape: {
-         cx: 100,
-         cy: 100,
-         r: 0
-      },
-      style: {
-         fill: 'blue'
-      }
-   });
+function draw(root) {
+    let lngLat = [39.5, 119];
+    // let pixel = 
+    let c = new Circle({
+        shape: {
+            cx: 100,
+            cy: 100,
+            r: 0
+        },
+        style: {
+            fill: 'blue'
+        }
+    });
 
-   c.animateShape(true)
-      .when(2000, {
-         r: 50
-      })
-      .start();
+    c.animateShape(true)
+        .when(5000, {
+            r: 50
+        })
+        .start();
 
-   zr.add(c);
+    root.add(c);
 }
